a975cef2a4c8e61ee1c009b4c36bc84ac23998a1
Park Shinhee <mam2zyo@gmail.com>
2025-06-16 17:09:01 +0900

    StringEx10

--- src/string/ex/StringEx10.java
+++ src/string/ex/StringEx10.java
@@ -42,6 +42,7 @@ package string.ex;
 */
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StringEx10 {
     public static int countChar(String text) {
@@ -52,13 +53,7 @@ public class StringEx10 {
         String cleaned = text.replaceAll("[?!.]", "").toLowerCase();
         String[] array = cleaned.split(" ");
 
-        Set<String> words = new HashSet<>();
-
-        for (String word : array) {
-            if (!words.contains(word)) {
-                words.add(word);
-            }
-        }
+        Set<String> words = new HashSet<>(Arrays.asList(array));
         return words.size();
     }
 
@@ -68,9 +63,12 @@ public class StringEx10 {
     }
 
     public static Map<Character, Integer> getCharacterFrequency(String text) {
+
+        String cleaned = text.replaceAll("[ .]", "").toLowerCase();
+
         Map<Character, Integer> frequency = new HashMap<>();
 
-        for (char c : text.toCharArray()) {
+        for (char c : cleaned.toCharArray()) {
             frequency.put(c, frequency.getOrDefault(c, 0) + 1);
         }
 
@@ -79,7 +77,7 @@ public class StringEx10 {
 
     public static String getLongestWord(String text) {
         String cleaned = text.replaceAll("[?!.]", "").toLowerCase();
-        String[] words = cleaned.split("");
+        String[] words = cleaned.split(" ");
 
         String longest = words[0];
 
@@ -94,7 +92,7 @@ public class StringEx10 {
 
     public static String getShortestWord(String text) {
         String cleaned = text.replaceAll("[?!.]", "").toLowerCase();
-        String[] words = cleaned.split("");
+        String[] words = cleaned.split(" ");
 
         String shortest = words[0];
 
@@ -103,14 +101,13 @@ public class StringEx10 {
                 shortest = words[i];
             }
         }
-
         return shortest;
     }
 
-    public static List<String> getPalindrome(String text) {
+    public static Set<String> getPalindromes(String text) {
         String cleaned = text.replaceAll("[?!.]", "").toLowerCase();
-        String[] words = cleaned.split("");
-        List<String> palindromes = new ArrayList<>();
+        String[] words = cleaned.split(" ");
+        Set<String> palindromes = new HashSet<>();
 
         for (String word : words) {
             if (isPalindrome(word)) {
@@ -121,18 +118,10 @@ public class StringEx10 {
     }
 
     private static boolean isPalindrome (String word) {
-        int left = 0;
-        int right = word.length() -1;
+        StringBuilder origin = new StringBuilder(word);
+        String reversed = origin.reverse().toString();
 
-        while (left < right) {
-            if (word.charAt(left) != word.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
+        return word.equals(reversed);
     }
 
 
@@ -143,42 +132,57 @@ public class StringEx10 {
 
         StringBuilder report = new StringBuilder();
 
-        report.append("=== 문자열 분석 보고서 ===\n")
-                .append("총 문자 수: ").append(countChar(text)).append("\n")
-                .append("총 단어 수: ").append(countWord(text)).append("\n")
-                .append("총 문장 수: ").append(countSentence(text)).append("\n")
+        report.append("=== 문자열 분석 보고서 ===")
+                .append("\n총 문자 수: ").append(countChar(text))
+                .append("\n총 단어 수: ").append(countWord(text))
+                .append("\n총 문장 수: ").append(countSentence(text))
                 .append("\n");
 
-        report.append("가장 많이 사용된 문자:")
-                .append("");
+        Map<Character, Integer> charCountMap = getCharacterFrequency(text);
 
-//        가장 많이 사용된 문자:
-//        1. 'a' (12회)
-//        2. 'r' (10회)
-//        3. 'g' (8회)
+        Map<Character, Integer> sortedMap = charCountMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .limit(3)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new));
+
+        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(sortedMap.entrySet());
+
+        String firstChar = String.format("1. %c (%d회)",
+                entryList.get(0).getKey(), entryList.get(0).getValue());
+        String secondChar = String.format("2. %c (%d회)",
+                entryList.get(1).getKey(), entryList.get(1).getValue());
+        String thirdChar = String.format("3. %c (%d회)",
+                entryList.get(2).getKey(), entryList.get(2).getValue());
+
+        report.append("\n가장 많이 사용된 문자:")
+                .append("\n").append(firstChar)
+                .append("\n").append(secondChar)
+                .append("\n").append(thirdChar)
+                .append("\n");
 
 
         String longest = getLongestWord(text);
-        String shortest = getShortestWord(text);
-        String formatted1 = String.format("가장 긴 단어: %s (%d글자)\n",
+        String longestFormat = String.format("\n가장 긴 단어: %s (%d글자)",
                 longest, longest.length());
-        String formatted2 = String.format("가장 짧은 단어: %s (%d글자)\n",
+        String shortest = getShortestWord(text);
+        String shortestFormat = String.format("\n가장 짧은 단어: %s (%d글자)",
                 shortest, shortest.length());
 
-        report.append("단어 분석:\n")
-                .append(formatted1).append("\n")
-                .append(formatted2).append("\n")
+        report.append("\n단어 분석:")
+                .append(longestFormat)
+                .append(shortestFormat)
                 .append("\n");
 
-        String palindromes =
-        report.append("회문 단어: ").append()
+        Set<String> palindromes = getPalindromes(text);
+        String joined = String.join(" ", palindromes);
 
-//
-//        회문 단어: level
-//
-//        보고서 생성 완료!
+        report.append("\n회문 단어: ").append(joined);
 
+        report.append("\n보고서 생성 완료!");
 
+        System.out.println(report.toString());
     }
-
 }
